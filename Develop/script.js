3564dd48e5973a2435d808c474a9ae120e531998
janae.wel <janae.wel@gmail.com>
2022-06-26 10:06:02 -0400

    starting moment.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,3 +1,10 @@
 var main = $(".schedule-block");
 
 var rowDiv = $("div").addClass("row").append(main);
+
+
+var m = moment(date, "LLL").format("dddd, MMMM Do YYYY");
+console.log(m);
+
+
+$(".date").text() = m;
